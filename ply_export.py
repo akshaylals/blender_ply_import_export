c83b61b64c8c757771fac0cca37150ada30d1ec0
akshaylals <akshaylal.s@inapp.com>
2024-01-23 12:33:50 +0530

    implemented face colors

--- ply_export.py
+++ ply_export.py
@@ -18,18 +18,15 @@ import numpy as np
 from plyfile import PlyData, PlyElement
 
 
-# int(round(0.44*255, 0))
-
 def write_some_data(context, filepath, format_ascii):
     obj = bpy.context.active_object
+    mats = obj.data.materials
 
     vertices = [ (vertex.co.x, vertex.co.y, vertex.co.z, vertex.normal.x, vertex.normal.y, vertex.normal.z) for vertex in obj.data.vertices ]
-    faces = [ (tuple([ vertex for vertex in face.vertices ]), face.normal.x, face.normal.y, face.normal.z) for face in obj.data.polygons ]
+    faces = [ tuple([tuple([ vertex for vertex in face.vertices ]), face.normal.x, face.normal.y, face.normal.z] + [ int(round(i*255, 0)) for i in mats[face.material_index].node_tree.nodes["Principled BSDF"].inputs["Base Color"].default_value ]) for face in obj.data.polygons ]
 
-    # vertex = PlyElement.describe(np.array(vertices, dtype=[('x', 'f4'), ('y', 'f4'), ('z', 'f4'), ('nx', 'f4'), ('ny', 'f4'), ('nz', 'f4')]), 'vertex')
-    # face = PlyElement.describe(np.array(faces, dtype=[('vertex_indices', 'i4', (3,)), ('fx', 'f4'), ('fy', 'f4'), ('fz', 'f4')]), 'face')
     vertex = PlyElement.describe(np.array(vertices, dtype=[('x', 'f4'), ('y', 'f4'), ('z', 'f4'), ('nx', 'f4'), ('ny', 'f4'), ('nz', 'f4')]), 'vertex')
-    face = PlyElement.describe(np.array(faces, dtype=[('vertex_indices', 'i4', (3,)), ('fx', 'f4'), ('fy', 'f4'), ('fz', 'f4')]), 'face')
+    face = PlyElement.describe(np.array(faces, dtype=[('vertex_indices', 'i4', (3,)), ('fx', 'f4'), ('fy', 'f4'), ('fz', 'f4'), ('red', 'u1'), ('green', 'u1'), ('blue', 'u1'), ('alpha', 'u1')]), 'face')
 
     with open(filepath, mode='wb') as f:
         PlyData([vertex, face], text=format_ascii).write(f)
